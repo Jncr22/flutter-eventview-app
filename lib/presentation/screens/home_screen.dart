2fe2c6cec3e557fd9457c0ddea0df0e6bcb213ad
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-05 14:26:44 -0500

    creacion basica del home

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -5,9 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
-      body: Center(
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('example'),
+      ),
+      body: const Center(
         child: Text('Hola mundo'),
       ),
     );
