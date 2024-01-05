5abf241d9c7e09db9ea060ce92b858485e182694
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-05 17:21:21 -0500

    Se crearon los screen de manera basica

---
+++ lib/presentation/screens/space_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpaceView extends StatelessWidget {
+  const SpaceView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('esapcios'),
+    );
+  }
+}
---
+++ lib/presentation/screens/tabscreen/circle_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CircleWord extends StatelessWidget {
+  const CircleWord({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("mundo"),
+    );
+  }
+}
