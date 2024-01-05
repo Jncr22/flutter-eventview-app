025ec8981617a64db0493bf3d4d29d5222b13007
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-05 17:22:04 -0500

    Se modifico el home

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('example'),
-      ),
-      body: const Center(
-        child: Text('Hola mundo'),
-      ),
+    return const Center(
+      child: Text("mundo"),
     );
   }
 }
