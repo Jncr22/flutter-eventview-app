196f7cec09c775028c7f3323c1ccd2366725de5c
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-05 14:28:30 -0500

    Se exportaron las pantallas

---
+++ lib/presentation/screens.dart
@@ -0,0 +1,6 @@
+export 'package:eventview_application_1/presentation/screens/login_screen.dart';
+export 'package:eventview_application_1/presentation/screens/register_screen.dart';
+
+export 'package:eventview_application_1/presentation/screens/home_screen.dart';
+
+
