38dae7588635680d7a0a283af947e09da79f2f6e
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-05 17:22:21 -0500

    Exportacion de todos los screens

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,6 +1,18 @@
+export 'package:eventview_application_1/presentation/screens/msg_screen.dart';
+export 'package:eventview_application_1/presentation/screens/resource_screen.dart';
+export 'package:eventview_application_1/presentation/screens/space_screen.dart';
+
+export 'package:eventview_application_1/presentation/screens/lobby_screen.dart';
+
+export 'package:eventview_application_1/presentation/screens/tabscreen/circle_screen.dart';
+export 'package:eventview_application_1/presentation/screens/tabscreen/events_screen.dart';
+export 'package:eventview_application_1/presentation/screens/tabscreen/news_screen.dart';
+export 'package:eventview_application_1/presentation/screens/tabscreen/store_screen.dart';
+export 'package:eventview_application_1/presentation/widgets/tabbar_widgets.dart';
 export 'package:eventview_application_1/presentation/screens/login_screen.dart';
 export 'package:eventview_application_1/presentation/screens/register_screen.dart';
-
 export 'package:eventview_application_1/presentation/screens/home_screen.dart';
+
+
 
 
