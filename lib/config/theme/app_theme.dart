d089617e7c045172d22027db0a728dfd0735046e
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-06 18:39:17 -0500

    Se agregaron los colores

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
 const colorList = <Color>[
-  Colors.black,
-  Colors.black12,
-  Colors.white,
-  Colors.grey,
-  Colors.purple
+ Color.fromRGBO(15, 22, 81, 1),
+ Color.fromRGBO(255, 189, 89, 1),
+ Color.fromRGBO(240, 221, 154, 1),
+ Color.fromRGBO(152, 160, 245, 1)
 ];
 
 class AppTheme {
-  final int selectedColor;
+ final int selectedColor;
 
-  AppTheme({this.selectedColor = 0})
-      : assert(selectedColor >= 0, 'selected color must be greater then 0'),
-      assert(selectedColor < colorList.length, 'selected color must be less or equal than ${colorList.length - 1 }');
+ AppTheme({this.selectedColor = 0})
+     : assert(selectedColor >= 0, 'selected color must be greater then 0'),
+       assert(selectedColor < colorList.length, 'selected color must be less or equal than ${colorList.length - 1 }');
 
-  ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor],
-  );
+ ThemeData getTheme() => ThemeData(
+   useMaterial3: true,
+   colorSchemeSeed: colorList[selectedColor],
+   appBarTheme: const AppBarTheme(
+     backgroundColor: Color.fromRGBO(15, 22, 81, 1), // Fondo de la AppBar
+     titleTextStyle: TextStyle(color: Colors.white), // Color del título de la AppBar
+     iconTheme: IconThemeData(color: Colors.white), // Color de los iconos de la AppBar
+   ),
+   bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+     backgroundColor: Color.fromRGBO(15, 22, 81, 1), // Fondo de la BottomNavigationBar
+     selectedItemColor: Colors.white, // Color del ítem seleccionado
+     unselectedItemColor: Colors.black, // Color de los ítems no seleccionados
+   ),
+ );
+
+ 
+ 
 }
