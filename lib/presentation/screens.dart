3416d7b073b44706f03cd589fec4d7a64d01d0ab
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-06 18:36:19 -0500

    Se agrego la pantalla de inicio

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,9 +1,9 @@
+export 'package:eventview_application_1/presentation/screens/start_screen.dart';
+
 export 'package:eventview_application_1/presentation/screens/msg_screen.dart';
 export 'package:eventview_application_1/presentation/screens/resource_screen.dart';
 export 'package:eventview_application_1/presentation/screens/space_screen.dart';
-
 export 'package:eventview_application_1/presentation/screens/lobby_screen.dart';
-
 export 'package:eventview_application_1/presentation/screens/tabscreen/circle_screen.dart';
 export 'package:eventview_application_1/presentation/screens/tabscreen/events_screen.dart';
 export 'package:eventview_application_1/presentation/screens/tabscreen/news_screen.dart';
@@ -12,7 +12,3 @@ export 'package:eventview_application_1/presentation/widgets/tabbar_widgets.dart
 export 'package:eventview_application_1/presentation/screens/login_screen.dart';
 export 'package:eventview_application_1/presentation/screens/register_screen.dart';
 export 'package:eventview_application_1/presentation/screens/home_screen.dart';
-
-
-
-
