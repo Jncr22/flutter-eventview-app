778978e98457e307b7b55040816ce77c680fcb79
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-06 18:39:40 -0500

    Se agreo el appbar con el logo

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/logo_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("mundo"),
-    );
+    return Scaffold(appBar: AppBar(
+        title: const Align(
+          alignment: Alignment.center,
+          child: LogoView(fontSize: 24.0,
+          ),
+          )
+        )
+      );
   }
 }
