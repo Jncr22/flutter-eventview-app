42eb9badc8b1777993cd93dd9e40062d0d513465
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-06 18:36:50 -0500

    Se agrego los colores

--- lib/presentation/screens/lobby_screen.dart
+++ lib/presentation/screens/lobby_screen.dart
@@ -28,45 +28,52 @@ class _LobyScreen extends State<LobyScreen> with SingleTickerProviderStateMixin 
  Widget build(BuildContext context) {
   WidgetOptions widgetOptions = WidgetOptions(selectedIndex: _selectedIndex, tabController: _tabController);
   return Scaffold(
-    body: Center(
-      child: widgetOptions.options.elementAt(_selectedIndex),
+   body: Center(
+     child: widgetOptions.options.elementAt(_selectedIndex),
+   ),
+   bottomNavigationBar: Theme(
+    data: Theme.of(context).copyWith(
+     canvasColor: const Color.fromRGBO(15, 22, 81, 1), // Fondo de la BottomNavigationBar
+     primaryColor: Colors.white, // Color del ítem seleccionado
+     //textTheme: Theme.of(context).textTheme.copyWith(caption: const TextStyle(color: Colors.white)), // Color de los ítems no seleccionados
     ),
-    bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-         BottomNavigationBarItem(
-     icon: Icon(Icons.home, color: Colors.black),
-     label: 'Home',
-     activeIcon: Icon(Icons.home, color: Colors.purple),
+    child: BottomNavigationBar(
+     items: const <BottomNavigationBarItem>[
+       BottomNavigationBarItem(
+         icon: Icon(Icons.home),
+         label: 'Home',
+         activeIcon: Icon(Icons.home,color: Colors.white),
+       ),
+       BottomNavigationBarItem(
+         icon: Icon(Icons.business),
+         label: 'Business',
+         activeIcon: Icon(Icons.business,color: Colors.white),
+       ),
+       BottomNavigationBarItem(
+         icon: Icon(Icons.school),
+         label: 'School',
+         activeIcon: Icon(Icons.school,color: Colors.white),
+       ),
+       BottomNavigationBarItem(
+         icon: Icon(Icons.settings),
+         label: 'Settings',
+         activeIcon: Icon(Icons.settings,color: Colors.white),
+       ),
+       BottomNavigationBarItem(
+         icon: Icon(Icons.person),
+         label: 'Profile',
+         activeIcon: Icon(Icons.person,color: Colors.white),
+       ),
+     ],
+     currentIndex: _selectedIndex,
+     onTap: (index) {
+       setState(() {
+         _selectedIndex = index;
+       });
+     },
    ),
-   BottomNavigationBarItem(
-     icon: Icon(Icons.business, color: Colors.black),
-     label: 'Business',
-     activeIcon: Icon(Icons.business, color: Colors.purple),
-   ),
-   BottomNavigationBarItem(
-     icon: Icon(Icons.school, color: Colors.black),
-     label: 'School',
-     activeIcon: Icon(Icons.school, color: Colors.purple),
-   ),
-   BottomNavigationBarItem(
-     icon: Icon(Icons.settings, color: Colors.black),
-     label: 'Settings',
-     activeIcon: Icon(Icons.settings, color: Colors.purple),
-   ),
-   BottomNavigationBarItem(
-     icon: Icon(Icons.person, color: Colors.black),
-     label: 'Profile',
-     activeIcon: Icon(Icons.person, color: Colors.purple),
-   ),
-      ],
-      currentIndex: _selectedIndex,
-      onTap: (index) {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
-    ),
-  );
+ ),
+ );
  }
 }
 
