f24f23f927d4a80eaa4970c6a9e0a79eb2863396
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-10 13:26:32 -0500

    Se modifico y se creo las carpetas de estudiante y profesor

--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,13 +1,13 @@
 export 'package:eventview_application_1/presentation/screens/start_screen.dart';
 
-export 'package:eventview_application_1/presentation/screens/msg_screen.dart';
-export 'package:eventview_application_1/presentation/screens/resource_screen.dart';
-export 'package:eventview_application_1/presentation/screens/space_screen.dart';
-export 'package:eventview_application_1/presentation/screens/lobby_screen.dart';
-export 'package:eventview_application_1/presentation/screens/tabscreen/circle_screen.dart';
-export 'package:eventview_application_1/presentation/screens/tabscreen/events_screen.dart';
-export 'package:eventview_application_1/presentation/screens/tabscreen/news_screen.dart';
-export 'package:eventview_application_1/presentation/screens/tabscreen/store_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/msg_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/resource_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/space_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/lobby_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/tabscreenTeacher/circle_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/tabscreenTeacher/events_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/tabscreenTeacher/news_screen.dart';
+export 'package:eventview_application_1/presentation/screens/teacher/tabscreenTeacher/store_screen.dart';
 export 'package:eventview_application_1/presentation/widgets/tabbar_widgets.dart';
 export 'package:eventview_application_1/presentation/screens/login_screen.dart';
 export 'package:eventview_application_1/presentation/screens/register_screen.dart';
