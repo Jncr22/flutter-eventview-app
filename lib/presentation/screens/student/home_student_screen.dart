48dda2520eabf2c968f14a69fc976924756a0842
Jose Carrillo <angel224.jncr10@gmail.com>
2024-01-12 11:13:53 -0500

    Creacion del home para estudiante

---
+++ lib/presentation/screens/student/home_student_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HomeStudentView extends StatelessWidget {
+  const HomeStudentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Homestudent');
+  }
+}
