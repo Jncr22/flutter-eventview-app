d5ce13f15c3d1a64f337aaed57b40e15004f566e
Jose Carrillo <angel224.jncr10@gmail.com>
2024-02-19 19:02:03 -0500

    feat -  se agrego codigo para evitar la rotacio de la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:eventview_application_1/config/router/app_router.dart';
 import 'package:eventview_application_1/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 void main() {
-  runApp(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();//para evitar la rotacio de la aplicacion
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
