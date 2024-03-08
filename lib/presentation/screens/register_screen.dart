a212a1f52f0db0a0c7cdc6a6f3cbb30ab8436db9
Jose Carrillo <angel224.jncr10@gmail.com>
2024-03-08 09:19:38 -0500

    rework - se recreo el registro estatico

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eventview_application_1/presentation/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,45 +13,100 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Nombre'),
+    return GradientBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Nombre',
+                  hintText: 'Nombre',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.text
+                  ),
+                 const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Apellido',
+                  hintText: 'Primer Apellido',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.text
+                  ),
+                 const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Apellidos',
+                  hintText: 'Segundo Apellido',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.text
+                  ),
+                 const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Nomina/Matricula',
+                  hintText: 'Matricula',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.number
+                  ),
+                 const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Categoria',
+                  hintText: 'Elige una opcion',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.emailAddress
+                  ),
+                   const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Telefono',
+                  hintText: 'Telefono',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.number
+                  ),
+                   const SizedBox(height:  30.0), 
+                const TextFieldCustom(
+                  labelText: 'Correo',
+                  hintText: 'Matricula',
+                  fontSize:  16.0,
+                   constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.emailAddress
+                  ),
+                  const SizedBox(height:  16.0),           
+                 const TextFieldCustom(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña',
+                  fontSize:  16.0,
+                  constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  ),
+                  const SizedBox(height:  16.0),           
+                 const TextFieldCustom(
+                  labelText: 'Contraseña',
+                  hintText: 'Contraseña',
+                  fontSize:  16.0,
+                  constraints: BoxConstraints(minWidth:  100, maxWidth:  350), // Define restricciones de tamaño
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Aquí va la lógica de registro
+                    context.goNamed('lobby');
+                    //Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: const Text('Registrarse'),              
+                ),
+              ],
             ),
-            TextField(
-              controller: lastNameController,
-              decoration: const InputDecoration(labelText: 'Apellido'),
-            ),
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(labelText: 'Correo'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(labelText: 'Contraseña'),
-              obscureText: true,
-            ),
-            TextField(
-              controller: confirmPasswordController,
-              decoration:
-                  const InputDecoration(labelText: 'Confirmar Contraseña'),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Aquí va la lógica de registro
-                context.goNamed('lobby');
-                //Navigator.pushReplacementNamed(context, '/home');
-              },
-              child: const Text('Registrarse'),              
-            ),
-          ],
+          ),
         ),
       ),
     );
