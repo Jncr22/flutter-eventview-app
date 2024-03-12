a61cf2ccd4f7778523c3f8ba17e709cf151c71f4
AlejandroV <2002433@utrivieramaya.edu.mx>
2024-03-11 20:14:17 -0500

    feat se creo la vista de espacios profesores

--- lib/presentation/screens/teacher/space_screen.dart
+++ lib/presentation/screens/teacher/space_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:eventview_application_1/presentation/widgets/gradien_bacground.dart';
 
 class SpaceView extends StatelessWidget {
   const SpaceView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('espacios'),
-    );
+    return GradientBackground(
+        child: Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(),
+      body: const Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                  child: Text(
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                      '¡Nuevas ')),
+              Text(
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                  'funciones'),
+              Text(
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                  'próximamente!')
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
