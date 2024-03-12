4403e55195c22d5f444d9c3c0946f157b593458b
AlejandroV <2002433@utrivieramaya.edu.mx>
2024-03-11 20:12:51 -0500

    feat se creo la vista home profesor

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,19 +1,66 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/logo_widget.dart';
+import 'package:eventview_application_1/presentation/screens.dart';
+import 'package:eventview_application_1/presentation/widgets/logo_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: const Align(
-      alignment: Alignment.center,
-      child: LogoView(
-        fontSize: 24.0,
+    return GradientBackground(
+        child: Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: const Align(
+          alignment: Alignment.centerLeft,
+          child: LogoView(),
+        ),
       ),
-    )));
+      body: const Stack(
+        children: [
+          IconsButtonDownBar(
+            leftButtonIcon:
+                Icons.settings, // Icono personalizado para el botón izquierdo
+            rightButtonIcon:
+                Icons.exit_to_app, // Icono personalizado para el botón derecho
+            iconSize: 40.0,
+          ),
+          Center(
+            widthFactor: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle_sharp, size: 150),
+                  SizedBox(height: 30.0),
+                  TextFieldCustom(
+                      labelText: 'Nombre',
+                      hintText: 'Isai Chi Lanestoza',
+                      fontSize: 16.0,
+                      constraints: BoxConstraints(
+                          minWidth: 100,
+                          maxWidth: 350), // Define restricciones de tamaño
+                      keyboardType: TextInputType.emailAddress),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(),
+                      TextFieldCustom(
+                          labelText: 'Nómina',
+                          hintText: '99999',
+                          fontSize: 16.0,
+                          constraints:
+                              BoxConstraints(minWidth: 100, maxWidth: 350),
+                          keyboardType: TextInputType.number),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
