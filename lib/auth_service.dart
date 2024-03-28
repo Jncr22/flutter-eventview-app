f2d21b2b6bfaf67a375bbf21d1fe11874bf0eb15
Jose Carrillo <angel224.jncr10@gmail.com>
2024-03-28 10:48:42 -0500

    feat - se agrego a¿la opcion de google como estudiante

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -4,12 +4,12 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService {
- final FirebaseAuth _auth = FirebaseAuth.instance;
- final GoogleSignIn _googleSignIn = GoogleSignIn();
- final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
- // Método para iniciar sesión
- Future<UserCredential?> signIn(String email, String password) async {
+  // Método para iniciar sesión
+  Future<UserCredential?> signIn(String email, String password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
@@ -20,12 +20,14 @@ class AuthService {
       print(e.message);
       return null;
     }
- }
+  }
 
   // Método para registrarse
- Future<UserCredential?> signUp(String email, String password, String name, String lastName, String category) async {
+  Future<UserCredential?> signUp(String email, String password, String name,
+      String lastName, String category) async {
     try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -41,10 +43,11 @@ class AuthService {
       print(e.message);
       return null;
     }
- }
+  }
 
- // Método para iniciar sesión con Google
- Future<UserCredential?> signInWithGoogle() async {
+  // Método para iniciar sesión con Google
+  // Método para iniciar sesión con Google
+  Future<UserCredential?> signInWithGoogle() async {
     try {
       // Inicia el proceso de autenticación con Google
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -53,7 +56,8 @@ class AuthService {
       if (googleUser == null) return null;
 
       // Obtiene las credenciales de Google
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
       // Crea una credencial de Firebase con el token de Google
       final AuthCredential credential = GoogleAuthProvider.credential(
@@ -62,10 +66,25 @@ class AuthService {
       );
 
       // Inicia sesión en Firebase con las credenciales de Google
-      return await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+
+      // Obtiene el nombre y apellido del usuario de Google
+      String name = googleUser.displayName!.split(' ')[0];
+      String lastName = googleUser.displayName!.split(' ')[1];
+
+      // Guarda la información adicional en Firestore
+      await _firestore.collection('users').doc(userCredential.user!.uid).set({
+        'name': name,
+        'lastName': lastName,
+        'email': userCredential.user!.email,
+        'category': 'Alumno', // Asigna la categoría de "Alumno" automáticamente
+      });
+
+      return userCredential;
     } catch (e) {
       print(e);
       return null;
     }
- }
+  }
 }
